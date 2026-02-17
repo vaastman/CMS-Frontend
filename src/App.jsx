ea06b146e4cf2f44928a25422922f9b92a0e6fcc
rk-rohiit <askrohiitsharma@gmail.com>
2026-02-17 23:30:36 +0530

    seperate landing page and admin section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-// import Header from "./components/Header";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
 import AppRoutes from "./routes/AppRoutes";
 import ScrollToTopButton from "./components/common/ScrollToTopButton";
 
 function App() {
   return (
     <>
-      {/* <Header />
-      <Navbar /> */}
       <AppRoutes />
-      {/* <Footer /> */}
 
-      {/* GLOBAL SCROLL TO TOP BUTTON */}
       <ScrollToTopButton />
     </>
   );
