31c6ea19fde50fba5b83d4809a5498b0b6608fe6
Devashish Kumar <dk3127197@gmail.com>
2026-05-14 12:02:09 +0530

    semester and fee updated as per requirement

--- src/utils/certificate.constants.js
+++ src/utils/certificate.constants.js
@@ -32,34 +32,36 @@ export const COURSE_OPTIONS = [
 
 // Valid Semesters
 export const SEMESTER_OPTIONS = [
-  // { value: '1st', label: '1st Semester' },
-  // { value: '2nd', label: '2nd Semester' },
-  // { value: '3rd', label: '3rd Semester' },
-  // { value: '4th', label: '4th Semester' },
-  // { value: '5th', label: '5th Semester' },
-  // { value: '6th', label: '6th Semester' },
-  // { value: '7th', label: '7th Semester' },
-   { value: '8th', label: '8th Semester' },
+  { value: '1st', label: '1st Semester' },
+  { value: '2nd', label: '2nd Semester' },
+  { value: '3rd', label: '3rd Semester' },
+  { value: '4th', label: '4th Semester' },
+  { value: '5th', label: '5th Semester' },
+  { value: '6th', label: '6th Semester' },
+  { value: '7th', label: '7th Semester' },
+  { value: '8th', label: '8th Semester' },
+  { value: 'part_3rd', label: 'Part 3rd' },
+
 ];
 
 // Valid Sessions
 export const SESSION_OPTIONS = [
   { value: '2022-2025', label: '2022-2025' },
-  // { value: '2023-2026', label: '2023-2026' },
-  // { value: '2024-2027', label: '2024-2027' },
-  // { value: '2025-2028', label: '2025-2028' },
+  { value: '2023-2027', label: '2023-2027' },
+  { value: '2024-2028', label: '2024-2028' },
+  { value: '2025-2029', label: '2025-2029' },
 ];
 
 // Certificate Types with Fees
 export const CERTIFICATE_TYPES = [
-  { value: 'BONAFIDE', label: 'Bonafide Certificate', fee: 200 },
-  { value: 'CLC', label: 'College Leaving Certificate (CLC)', fee: 500 },
-  { value: 'CHARACTER', label: 'Character Certificate', fee: 300 },
+  { value: 'BONAFIDE', label: 'Bonafide Certificate', fee: 100 },
+  { value: 'CLC', label: 'CLC + CHARACTER Certificate', fee: 500 },
+  // { value: 'CHARACTER', label: 'Character Certificate', fee: 300 },
 ];
 
 // Certificate Fees Map
 export const CERTIFICATE_FEES = {
-  BONAFIDE: 200,
+  BONAFIDE: 100,
   CLC: 500,
-  CHARACTER: 300,
+  // CHARACTER: 300,
 };
